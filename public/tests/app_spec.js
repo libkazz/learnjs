6987d061e885eaf099e466fe5e0e1a43123f9771
Kazuaki Takemoto <libkazz@gmail.com>
2018-08-01 00:18:25 +0900

    Exec 2.3

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,5 +1,9 @@
 describe('LearnJS', function() {
   it('can show a problem view', function() {
     learnjs.showView('#problem-1');
-    expect($('.view-container .problem-view').length).toEqual(1); });
+    expect($('.view-container .problem-view').length).toEqual(1); }
+  );
+  it('shows the landing page view when there is no hash', function() { learnjs.showView('');
+    expect($('.view-container .landing-view').length).toEqual(1);
+  });
 });
