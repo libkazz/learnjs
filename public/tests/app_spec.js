b1c0be4880a4acaffa92d644d8479b81d8dea45d
Kazuaki Takemoto <libkazz@gmail.com>
2018-08-01 00:03:38 +0900

    Exec 2.2.1

---
+++ public/tests/app_spec.js
@@ -0,0 +1,5 @@
+describe('LearnJS', function() {
+  it('can show a problem view', function() {
+    learnjs.showView('#problem-1');
+    expect($('.view-container .problem-view').length).toEqual(1); });
+});
